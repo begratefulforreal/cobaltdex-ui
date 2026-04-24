0a72698049d04a5c37600860133f1864489cfdd9
begreatfulforreal <192963054+begreatfulforreal@users.noreply.github.com>
2026-04-24 10:14:07 +0200

    s

--- src/store/useAppStore.ts
+++ src/store/useAppStore.ts
@@ -350,7 +350,7 @@ export const useAppStore = createStore<AppState>(
           const readyRpcs = [...rpcs]
           if (localRpcNode?.rpcNode) readyRpcs.sort((a, b) => (a.name === localRpcNode.rpcNode!.name ? -1 : 1))
           const success = await setRpcUrlAct(
-            'https://mainnet.helius-rpc.com/?api-key=78947ff9-3d63-4e85-a0f2-5c39fa66a3a5',
+            'https://solana-mainnet.core.chainstack.com/390e9c8ba27078293df143004c1c5003',
             true,
             i !== readyRpcs.length - 1
           )
